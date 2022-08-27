518a9bb0ca19fbabc1745db46981c67cc573c1f2
Oleg Repalo <altwulf7@gmail.com>
2022-08-27 18:35:13 +0300

    Add some dock strings, change branch for tests

--- tests.py
+++ tests.py
@@ -1,8 +1,15 @@
+"""
+Base tests
+"""
+
+
 from src.main import password_generator
 
 
 def test_password_generator():
+    """
+    Test password length
+    """
     strong = 10
     password = password_generator(strong)
     assert len(password) == strong * 2
-
