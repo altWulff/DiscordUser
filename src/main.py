0ee24d604899500cf82c28bed06e7cf0a295e107
Oleg Repalo <altwulf7@gmail.com>
2022-08-28 09:39:45 +0300

    Update README.MD, requirements.txt, GH actions

--- src/main.py
+++ src/main.py
@@ -3,13 +3,14 @@ Automatically creates a discord account and login
 into it, after receiving an authorization token
 from the headers "authorization"
 """
-import secrets
 import argparse
+import secrets
 from time import sleep
-from user import SeleniumUser
+
 from pyfiglet import Figlet
 from rich.console import Console
 
+from .user import SeleniumUser
 
 TITLE = "User Generator"
 
@@ -27,9 +28,23 @@ def parse_args():
     Args from command line,
     Email, and Nickname
     """
-    parser = argparse.ArgumentParser(description='Nickname and Email for new discord account.')
-    parser.add_argument('-username', dest="username", type=str, help='User name of new discord account', required=True)
-    parser.add_argument('-email', dest="email", type=str, help='Email name of new discord account', required=True)
+    parser = argparse.ArgumentParser(
+        description="Nickname and Email for new discord account."
+    )
+    parser.add_argument(
+        "-username",
+        dest="username",
+        type=str,
+        help="User name of new discord account",
+        required=True,
+    )
+    parser.add_argument(
+        "-email",
+        dest="email",
+        type=str,
+        help="Email name of new discord account",
+        required=True,
+    )
 
     args = parser.parse_args()
     return args.username, args.email
@@ -37,25 +52,24 @@ def parse_args():
 
 def main():
     """
-    Start function, 
-    call's functions 
+    Start function,
+    call's functions
     """
     console = Console()
-    custom_fig = Figlet(font="graffiti")
+    custom_fig = Figlet(font="colossal")
     console.print(custom_fig.renderText(TITLE))
 
     args = parse_args()
     user_name, email = args
     password = password_generator()
+    console.print(f"User: {user_name}\nEmail: {email}\nPassword: {password}")
     user = SeleniumUser(email, user_name, password)
     user.register()
-    user.login()
+    sleep(30)
     console.print(f"User has token: {user.get_token()}")
-    sleep(5)
     console.print("Press Enter or Return to exit ...")
 
 
 if __name__ == "__main__":
-
     main()
     input()
--- src/user.py
+++ src/user.py
@@ -1,67 +1,66 @@
 """Selenium User"""
-import json
 import random
+from datetime import datetime
+from time import sleep
 
 import requests
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver import ActionChains
 import undetected_chromedriver as webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from datetime import datetime
 from rich.console import Console
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class SeleniumUser:
     """Selenium User, auto register and login user on browser"""
-    def __init__(
-            self, email: str, username: str, password: str, url: str="https://discord.com"
-    ):
+
+    def __init__(self, email: str, username: str, password: str):
         self.email = email
         self.username = username
         self.password = password
-        self.url = url
+        self.url = "https://discord.com"
         self.driver = webdriver.Chrome()
         self.driver.set_window_position(0, 0)
         self.actions = ActionChains(self.driver)
-        self.wait = WebDriverWait(self.driver, 10)
 
         # requests session
         self.session = requests.Session()
-        selenium_user_agent = self.driver.execute_script(
-            "return navigator.userAgent;")
+        selenium_user_agent = self.driver.execute_script("return navigator.userAgent;")
         self.session.headers.update({"user-agent": selenium_user_agent})
         for cookie in self.driver.get_cookies():
-            self.session.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
+            self.session.cookies.set(
+                cookie["name"], cookie["value"], domain=cookie["domain"]
+            )
 
     def _fill_date_fields(self) -> None:
         """Filler date fields with random data"""
-        self.driver.find_elements(By.CLASS_NAME, 'css-1hwfws3')[0].click()
+        sleep(1.0)
+        self.driver.find_elements(By.CLASS_NAME, "css-1hwfws3")[0].click()
         self.actions.send_keys(str(random.randint(1, 12)))
         self.actions.send_keys(Keys.ENTER)
         self.actions.send_keys(str(random.randint(1, 28)))
         self.actions.send_keys(Keys.ENTER)
-        self.actions.send_keys(
-            str(random.randint(1990, 2001)))
+        self.actions.send_keys(str(random.randint(1990, 2001)))
         self.actions.send_keys(Keys.ENTER)
         self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.ENTER)
+        self.actions.perform()
 
     def _fill_reg_fields(self) -> None:
         """
         Filler user registration data: email, username, and password
         """
-        self.driver.find_element(By.NAME, 'email').send_keys(self.email)
-        self.driver.find_element(By.NAME, 'username').send_keys(self.username)
-        self.driver.find_element(By.NAME, 'password').send_keys(self.password)
+        self.driver.find_element(By.NAME, "email").send_keys(self.email)
+        self.driver.find_element(By.NAME, "username").send_keys(self.username)
+        self.driver.find_element(By.NAME, "password").send_keys(self.password)
         self._fill_date_fields()
         self.actions.send_keys(Keys.ENTER)
 
     def _fill_login_field(self) -> None:
         """Filler user login data: email and password"""
-        self.driver.find_element(By.NAME, 'email').send_keys(self.email)
-        self.driver.find_element(By.NAME, 'password').send_keys(self.password)
-        self.driver.find_element(By.NAME, 'password').send_keys(Keys.ENTER)
+        self.driver.find_element(By.NAME, "email").send_keys(self.email)
+        self.driver.find_element(By.NAME, "password").send_keys(self.password)
+        self.driver.find_element(By.NAME, "password").send_keys(Keys.ENTER)
 
     def register(self) -> None:
         """
@@ -79,40 +78,29 @@ class SeleniumUser:
         self.driver.get(f"{self.url}/login")
         self._fill_login_field()
 
-        payload = {
-            "login": self.email,
-            "password": self.password,
-            "undelete": "false",
-            "captcha_key": "null",
-            "login_source": "null",
-            "gift_code_sku_id": "null"
-        }
-        headers = {
-            ""
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-            'referer': 'https://www.google.com/'
-        }
-
-        #resp = self.session.post("https://discord.com/api/v9/auth/login", data=payload, headers=headers)
-        #print(resp, resp.headers)
-
     def get_token(self) -> str:
         """
         Get user token, after login
         """
-        current_url = self.driver.current_url
-        print(current_url)
-        self.wait.until(EC.url_changes(current_url))
-        self.wait.until(EC.url_to_be(f"{self.url}/channels/@me"))
-        current_url = self.driver.current_url
-        print(current_url)
-        token = self.driver.execute_script(f"return window.localStorage.getItem('token');")
-        return token
+        self.session.headers.update({"content-type": "application/json"})
+        payload = {
+            "login": self.email,
+            "password": self.password,
+            "undelete": False,
+            "captcha_key": "null",
+            "login_source": "null",
+            "gift_code_sku_id": "null",
+        }
+        response = self.session.post(f"{self.url}/api/v9/auth/login", data=payload)
+        self.log(f"Request status codee {response.status_code}")
+        try:
+            return response.json()["token"]
+        except AttributeError:
+            return "<None>"
 
     @staticmethod
     def log(message) -> None:
         """Messages log"""
         console = Console()
-        time = datetime.now().strftime('%H:%M:%S')
-        console.print(f'[{time}] MESSAGE: {message}')
-
+        time = datetime.now().strftime("%H:%M:%S")
+        console.print(f"[{time}] {message}")
